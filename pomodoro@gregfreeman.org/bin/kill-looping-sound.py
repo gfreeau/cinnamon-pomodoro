14c8c774e5c5bfa18e05615ffe1386bd63046591
gfreeau <greg@imagize.com.au>
2014-01-28 08:42:34 -0500

    Added -r to xargs to not run kill if no processes found

--- pomodoro@gregfreeman.org/bin/kill-looping-sound.py
+++ pomodoro@gregfreeman.org/bin/kill-looping-sound.py
@@ -7,6 +7,6 @@ from pipes import quote
 if (len(argv) != 2):
     exit(1)
 
-command = "ps aux | grep 'play -q' | grep %s | awk '{print $2}' | xargs kill -9" % (quote(argv[1]))
+command = "ps aux | grep 'play -q' | grep %s | awk '{print $2}' | xargs -r kill -9" % (quote(argv[1]))
 
 system(command)
